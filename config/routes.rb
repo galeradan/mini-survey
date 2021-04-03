ef53b733d132661022f25e590d8c6d727f9ff056
Dan Galera <danerick.galera@gmail.com>
2021-04-03 16:58:11 +0800

    Adds Survey controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match '/memberships' => 'survey#index',:via => [:get]
+
   resources :questions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
